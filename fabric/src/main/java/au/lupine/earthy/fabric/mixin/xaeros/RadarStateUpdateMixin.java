8543e410eedab074360315f1cf75deb38a0506d8
jwkerr <49851457+jwkerr@users.noreply.github.com>
2025-03-05 05:45:33 +1100

    Improve minimap adding

--- fabric/src/main/java/au/lupine/earthy/fabric/mixin/xaeros/RadarStateUpdateMixin.java
+++ fabric/src/main/java/au/lupine/earthy/fabric/mixin/xaeros/RadarStateUpdateMixin.java
@@ -1,5 +1,6 @@
 package au.lupine.earthy.fabric.mixin.xaeros;
 
+import au.lupine.earthy.fabric.EarthyFabric;
 import au.lupine.earthy.fabric.module.Session;
 import au.lupine.earthy.fabric.object.config.Config;
 import net.minecraft.client.Minecraft;
@@ -7,14 +8,8 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.player.RemotePlayer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.LightLayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,43 +17,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import xaero.hud.minimap.radar.state.RadarList;
 import xaero.hud.minimap.radar.state.RadarStateUpdater;
 
-import java.util.Set;
-
 @Mixin(value = RadarStateUpdater.class)
 public abstract class RadarStateUpdateMixin {
 
-    @Unique private static final Set<Block> TRANSPARENT_BLOCKS = Set.of(
-            Blocks.GLASS,
-            Blocks.WHITE_STAINED_GLASS, Blocks.LIGHT_GRAY_STAINED_GLASS, Blocks.GRAY_STAINED_GLASS, Blocks.BLACK_STAINED_GLASS,
-            Blocks.BROWN_STAINED_GLASS, Blocks.RED_STAINED_GLASS, Blocks.ORANGE_STAINED_GLASS, Blocks.YELLOW_STAINED_GLASS,
-            Blocks.LIME_STAINED_GLASS, Blocks.GREEN_STAINED_GLASS, Blocks.CYAN_STAINED_GLASS, Blocks.LIGHT_BLUE_STAINED_GLASS,
-            Blocks.BLUE_STAINED_GLASS, Blocks.PURPLE_STAINED_GLASS, Blocks.MAGENTA_STAINED_GLASS, Blocks.PINK_STAINED_GLASS,
-            Blocks.GLASS_PANE,
-            Blocks.WHITE_STAINED_GLASS_PANE, Blocks.LIGHT_GRAY_STAINED_GLASS_PANE, Blocks.GRAY_STAINED_GLASS_PANE, Blocks.BLACK_STAINED_GLASS_PANE,
-            Blocks.BROWN_STAINED_GLASS_PANE, Blocks.RED_STAINED_GLASS_PANE, Blocks.ORANGE_STAINED_GLASS_PANE, Blocks.YELLOW_STAINED_GLASS_PANE,
-            Blocks.LIME_STAINED_GLASS_PANE, Blocks.GREEN_STAINED_GLASS_PANE, Blocks.CYAN_STAINED_GLASS_PANE, Blocks.LIGHT_BLUE_STAINED_GLASS_PANE,
-            Blocks.BLUE_STAINED_GLASS_PANE, Blocks.PURPLE_STAINED_GLASS_PANE, Blocks.MAGENTA_STAINED_GLASS_PANE, Blocks.PINK_STAINED_GLASS_PANE,
-            Blocks.LANTERN, Blocks.SOUL_LANTERN,
-            Blocks.CHEST,
-            Blocks.IRON_BARS,
-            Blocks.TORCH, Blocks.SOUL_TORCH, Blocks.WALL_TORCH, Blocks.SOUL_WALL_TORCH,
-            Blocks.VINE, Blocks.GLOW_LICHEN,
-            Blocks.MOSS_CARPET, Blocks.SNOW,
-            Blocks.POINTED_DRIPSTONE,
-            Blocks.GRINDSTONE,
-            Blocks.DECORATED_POT,
-            Blocks.SCAFFOLDING,
-            Blocks.BREWING_STAND
-    );
-
-    @Unique private static final Set<TagKey<Block>> TRANSPARENT_BLOCK_TAGS = Set.of(
-            BlockTags.BANNERS, BlockTags.BUTTONS, BlockTags.TRAPDOORS, BlockTags.ALL_SIGNS,
-            BlockTags.PRESSURE_PLATES, BlockTags.FLOWERS, BlockTags.BEDS, BlockTags.AIR,
-            BlockTags.CAMPFIRES, BlockTags.CANDLES, BlockTags.CLIMBABLE, BlockTags.WOOL_CARPETS,
-            BlockTags.CROPS, BlockTags.DOORS, BlockTags.FLOWER_POTS, BlockTags.RAILS,
-            BlockTags.REPLACEABLE, BlockTags.SAPLINGS, BlockTags.ANVIL, BlockTags.FENCES,
-            BlockTags.WALLS, BlockTags.FENCE_GATES
-    );
+    @Unique private static final int HIDE_UNDER_LIGHT_LEVEL = 15;
 
     @Redirect(
             method = "update",
@@ -68,9 +30,7 @@ public abstract class RadarStateUpdateMixin {
             )
     )
     private boolean redirect(RadarList instance, Entity entity) {
-        Session session = Session.getInstance();
-
-        if (!session.isPlayerOnEarthMC()) return instance.add(entity);
+        if (!Session.getInstance().isPlayerOnEarthMC()) return instance.add(entity);
 
         if (!Config.showUnobscuredPlayersOnMap) return false;
 
@@ -78,44 +38,22 @@ public abstract class RadarStateUpdateMixin {
 
         Minecraft client = Minecraft.getInstance();
         LocalPlayer player = client.player;
-
         if (player == null) return instance.add(entity);
+
+        ClientLevel level = client.level;
+        if (level == null) return false;
 
         if (player.isSpectator()) return instance.add(other);
 
-        if (!session.isPlayerInOverworld()) return false;
+        if (!level.dimensionType().hasSkyLight()) return false;
 
-        ClientLevel level = client.level;
+        int brightness = level.getBrightness(LightLayer.SKY, BlockPos.containing(other.getEyePosition()));
+        if (brightness < HIDE_UNDER_LIGHT_LEVEL) return false;
 
-        if (shouldAddPlayer(level, player, other)) return instance.add(other);
-
-        return false;
-    }
-
-    @Unique
-    private boolean shouldAddPlayer(ClientLevel level, LocalPlayer player, Player other) {
-        if (other.isCrouching()) return false;
+        level.getSkyDarken();
         if (other.isInvisibleTo(player)) return false;
+        if (other.isCrouching()) return false;
 
-        return areBlocksAboveTransparent(level, other.position());
-    }
-
-    @Unique
-    private boolean areBlocksAboveTransparent(ClientLevel level, Vec3 position) {
-        int x = (int) position.x;
-        int z = (int) position.z;
-
-        nextBlock: for (int y = (int) Math.ceil(position.y); y <= level.getHeight(); y++) {
-            BlockPos currentPos = new BlockPos(x, y, z);
-            BlockState bs = level.getBlockState(currentPos);
-
-            for (TagKey<Block> tag : TRANSPARENT_BLOCK_TAGS) {
-                if (bs.is(tag)) continue nextBlock;
-            }
-
-            if (!TRANSPARENT_BLOCKS.contains(bs.getBlock())) return false;
-        }
-
-        return true;
+        return instance.add(other);
     }
 }
