68cf3dc7adae49609c1e7c6fe296ac44578d1374
jwkerr <49851457+jwkerr@users.noreply.github.com>
2025-05-02 09:44:52 +1000

    Changes

--- fabric/src/main/java/au/lupine/earthy/fabric/object/hook/ConditionalMixinPlugin.java
+++ fabric/src/main/java/au/lupine/earthy/fabric/object/hook/ConditionalMixinPlugin.java
@@ -2,8 +2,6 @@ package au.lupine.earthy.fabric.object.hook;
 
 import au.lupine.earthy.fabric.EarthyFabric;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.ModContainer;
-import net.fabricmc.loader.api.metadata.ModMetadata;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
