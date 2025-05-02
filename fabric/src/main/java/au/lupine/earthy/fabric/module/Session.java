564298cdedc6afe56cd8fddf499d72f4d9e6b99f
jwkerr <49851457+jwkerr@users.noreply.github.com>
2025-05-02 15:51:23 +1000

    Change

--- fabric/src/main/java/au/lupine/earthy/fabric/module/Session.java
+++ fabric/src/main/java/au/lupine/earthy/fabric/module/Session.java
@@ -54,16 +54,6 @@ public final class Session extends Module {
     public void authenticate() {
         CompletableFuture.runAsync(() -> {
             try {
-                UUID uuid = Minecraft.getInstance().getGameProfile().getId();
-
-                Player player = EarthyFabric.getAPI().getPlayerByUUID(uuid);
-                if (player == null) {
-                    authentication = Authentication.FAILED;
-                    return;
-                }
-
-                Instant instant = Instant.ofEpochMilli(player.getJoinedTownAt() == null ? 0 : player.getJoinedTownAt());
-
                 HttpResponse<String> response = HttpClient.newHttpClient().send(
                         HttpRequest.newBuilder().GET()
                                 .uri(URI.create("https://gist.githubusercontent.com/jwkerr/4b6fcbd438fc546f23e57210077b00ce/raw/authenticated.json"))
@@ -72,10 +62,17 @@ public final class Session extends Module {
                 );
 
                 String body = response.body();
+                UUID uuid = Minecraft.getInstance().getGameProfile().getId();
 
                 JsonObject object = JsonParser.parseString(body).getAsJsonObject();
-                if (isUUIDAuthenticated(uuid, object.get("users").getAsJsonArray(), instant)) {
+                if (isUUIDAuthenticated(uuid, object.get("users").getAsJsonArray(), null)) {
                     authentication = Authentication.AUTHENTICATED;
+                    return;
+                }
+
+                Player player = EarthyFabric.getAPI().getPlayerByUUID(uuid);
+                if (player == null) {
+                    authentication = Authentication.FAILED;
                     return;
                 }
 
@@ -83,6 +80,8 @@ public final class Session extends Module {
                 NationIdentifier nation = player.getNation();
 
                 if (town != null) {
+                    Instant instant = Instant.ofEpochMilli(player.getJoinedTownAt());
+
                     if (isUUIDAuthenticated(town.getUUID(), object.get("towns").getAsJsonArray(), instant)) {
                         authentication = Authentication.AUTHENTICATED;
                         return;
